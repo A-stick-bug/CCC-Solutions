bead52ffa8b874e3bd0c2517c17c9374b3dffaff
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-04-01 04:31:31 -0400

    added explanation for code

--- CCC '10 J4 - Global Warming.py
+++ CCC '10 J4 - Global Warming.py
@@ -1,3 +1,15 @@
+"""
+How the code works:
+First extract the sequence from the temperatures
+Test the following for every sequence length, starting from shortest and break when a working sequence is found
+(guarantees the found sequence to be shortest):
+    remove all occurrences of the sequence in the array
+    to test if the sequence works, check if the remaining part after removing all 
+    instances of the sequence is the start of the sequence
+
+Note: the flag variable is for in case there is no shorter sequence
+"""
+
 x = input().split()
 while x != ["0"]:
     x = list(map(int, x))
@@ -5,6 +17,7 @@ while x != ["0"]:
     length = x[0]
     temps = x[1:]
     sequence = []
+    # extract sequence
     for i in range(len(temps) - 1):
         sequence.append(temps[i + 1] - temps[i])
 
