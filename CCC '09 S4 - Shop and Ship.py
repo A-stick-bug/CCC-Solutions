5615345a3a1cfee24779f5b3dd26c9fd5daea26a
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-05-23 12:05:29 -0400

    passes all test cases

--- CCC '09 S4 - Shop and Ship.py
+++ CCC '09 S4 - Shop and Ship.py
@@ -1,55 +1,60 @@
-# TLE on test cases 4-6
-# the other solution for this question passes all test cases
+"""
+This problem has few nodes and A LOT of edges so V^2 is actually faster than E*log(V) with heap
 
-import heapq
-from collections import defaultdict
+Since the nodes are numbers 1 to N, we can use a list to keep track of shortest to a node where the value is the cost
+and the index is the node (no need for dictionaries)
 
-n = int(input())
-TRADE_ROUTES = int(input())
-
-graph = defaultdict(list)
-
-for i in range(TRADE_ROUTES):
-    node1, node2, cost = list(map(int, input().split()))
-    if 0 < node1 <= n and 0 < node2 <= n:
-        graph[node1].append((node2, cost))
-        graph[node2].append((node1, cost))
-
-p = int(input())
-pencils = {i: float('inf') for i in range(1,n+1)}
-for i in range(p):
-    pencil, cost = list(map(int, input().split()))
-    pencils[pencil] = cost
-
-END = int(input())
+However, we must add an extra index at the end because arrays indices go from 0 to N-1 and the nodes are numbered 1 to N
+Index 0 being there won't affect anything
+"""
 
 
-def shortest_path(start, graph):
-    costs = {node: float('inf') for node in range(1,n+1)}
-    costs[start] = 0
-    pq = [(0, start)]
-    processed = set()
+def dijkstra(graph, start):
+    n = len(graph)
+    visited = [False] * n
+    distances = [float('inf')] * n
+    distances[start] = 0
 
-    while pq:
-        cost, node = heapq.heappop(pq)
-        if node in processed:
-            continue
-
-        neighbors = graph[node]
-
-        if neighbors is not None:
-            for neighbor, neighbor_cost in neighbors:
-                new_cost = cost + neighbor_cost
-                if costs[neighbor] > new_cost:
-                    costs[neighbor] = new_cost
-                    heapq.heappush(pq, (new_cost, neighbor))
-        processed.add(node)
-    return costs
+    for _ in range(n):
+        min_distance = float('inf')
+        for i in range(n):
+            if not visited[i] and distances[i] < min_distance:
+                u = i
+                min_distance = distances[i]
+        visited[u] = True
+        for k in range(n):
+            if not visited[k] and graph[u][k] and distances[u] + graph[u][k] < distances[k]:
+                distances[k] = distances[u] + graph[u][k]
+    return distances
 
 
-min_cost = float('inf')
-dijkstra_result = shortest_path(END, graph)
-for node, cost in dijkstra_result.items():
-    if pencils[node] + cost < min_cost:
-        min_cost = pencils[node] + cost
-print(min_cost)
+n = int(input()) + 1
+t = int(input())
+
+# for some reason using float('inf') here is slower than 10**9 (greater than max cost possible)
+mat = [[10**9 for j in range(n)] for i in range(n)]
+
+# create the adjacency matrix
+for i in range(t):
+    d, b, c = map(int, input().split())
+    # undirected graph
+    mat[d][b] = c
+    mat[b][d] = c
+
+pencils = int(input())
+distances = [float('inf') for i in range(n)]  # distance to each node, the index being the node
+
+# pencil costs in tuple pairs (node, cost to buy pencil here)
+stores = []
+for _ in range(pencils):
+    stores.append(list(map(int, input().split())))
+
+start = int(input())
+distances = dijkstra(mat,start)
+
+# adding the node's cost
+pencil = float('inf')
+for d, c in stores:
+    pencil = min(pencil, distances[d] + c)
+
+print(pencil)
