df79f4162bd038d5327f47542d490108a8ce5ff5
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-08 21:56:31 -0400

    added comments

--- 2005/CCC '05 S5 - Pinball Ranking.cpp
+++ 2005/CCC '05 S5 - Pinball Ranking.cpp
@@ -10,13 +10,18 @@ int main() {
     std::cin >> n;
     std::vector<int> scores;
     double total = 0;
-    for (int i = 1; i <= n; i++) {
+
+    for (int i = 1; i <= n; i++) {  // i is how many elements are in scores during this iteration
         int score;
         std::cin >> score;
+
+        // find ranking of this score
         int rank = std::upper_bound(scores.begin(), scores.end(), score) - scores.begin();
         scores.insert(scores.begin() + rank, score);
-        total += i - rank;
+        total += i - rank;  // reverse rank because scores is in increasing order
     }
+
+    // get 2 decimal place average
     std::cout << std::fixed << std::setprecision(2) << total / n << std::endl;
     return 0;
 }
--- 2005/CCC '05 S5 - Pinball Ranking.py
+++ 2005/CCC '05 S5 - Pinball Ranking.py
@@ -7,10 +7,10 @@ from bisect import bisect_right
 n = int(input())
 scores = []
 total = 0
-for i in range(1, n + 1):
+for i in range(1, n + 1):  # i is how many elements are in scores during this iteration
     score = int(input())
-    rank = bisect_right(scores, score)
+    rank = bisect_right(scores, score)  # find ranking of this score
     scores.insert(rank, score)
-    total += i - rank
+    total += i - rank  # reverse rank because scores is in increasing order
 
-print(round(total / n, 2))
+print(round(total / n, 2))  # get 2 decimal place average
