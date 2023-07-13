e49f48b094fe453813a7ab32c8d6f08380b71637
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-13 19:21:41 -0400

    made solution easier to undestand

--- 2015/CCC '15 S4 - Convex Hull.py
+++ 2015/CCC '15 S4 - Convex Hull.py
@@ -12,18 +12,20 @@ for _ in range(n_routes):
 start, end = map(int, input().split())
 
 inf = 10 ** 9 + 1
-dist = [[inf, inf] for _ in range(n_islands + 1)]
-dist[start] = (0, 0)
+dist = [[inf]*201 for _ in range(n_islands + 1)]
+for i in range(201):
+    dist[start][i] = 0  # dist to start is always 0
+
 pq = [(0, start, 0)]
 
 while pq:
     time, island, damage = heapq.heappop(pq)
-    if island == end:
+    if island == end and damage < hull:
         print(time)
         sys.exit()
 
     # already processed this, can skip
-    if time > dist[island][0] and damage > dist[island][1]:
+    if time > dist[island][damage]:
         continue
 
     for adj, adj_time, adj_damage in graph[island]:
@@ -33,60 +35,8 @@ while pq:
         if new_damage >= hull:  # broken hull, can't take this path
             continue
 
-        if new_time < dist[adj][0] or new_damage < dist[adj][1]:
-            dist[adj][0] = new_time
-            dist[adj][1] = new_damage
+        if new_time < dist[adj][new_damage]:
+            dist[adj][new_damage] = new_time
             heapq.heappush(pq, (new_time, adj, new_damage))
 
 print(-1)
-
-
-# similar alternate solution
-# import heapq
-# import sys
-# from collections import defaultdict
-#
-# hull, n_islands, n_routes = map(int, input().split())
-#
-# # inside each defaultdict, the key is the destination node and the value is all the paths
-# graph = [defaultdict(list) for n in range(n_islands + 1)]
-# for m in range(n_routes):
-#     a, b, time, damage = map(int, input().split())
-#     graph[a][b].append((time, damage))
-#     graph[b][a].append((time, damage))
-#
-# inf = 10**9 + 1
-#
-# start, end = map(int, input().split())
-#
-# min_time = inf
-# distance = [[inf for _ in range(201)] for _ in range(n_islands+1)]
-#
-# for i in range(201):
-#     distance[start][i] = 0
-#
-# pq = [(0, start, hull)]
-#
-# visited = set()
-# while pq:
-#     cost, island, hp = heapq.heappop(pq)
-#
-#     if island == end:  # end reached
-#         print(cost)
-#         sys.exit()
-#
-#     if (island,hp) in visited:
-#         continue
-#     visited.add((island,hp))
-#
-#     for adj in graph[island]:  # for every island we can reach from teh current
-#         for adj_time, adj_damage in graph[island][adj]:  # process every route
-#             new_time = distance[island][hp] + adj_time
-#             new_hp = hp - adj_damage
-#
-#             if new_hp > 0:
-#                 if new_time < distance[adj][new_hp]:
-#                     distance[adj][new_hp] = new_time
-#                 heapq.heappush(pq, (new_time, adj, new_hp))
-#
-# print(-1)  # can't get to end
