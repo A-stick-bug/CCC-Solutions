40017bc4ee67a053dd1afb567624c60c2b48d460
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-19 17:05:08 -0400

    added comments

--- 2015/CCC '15 S4 - Convex Hull.py
+++ 2015/CCC '15 S4 - Convex Hull.py
@@ -1,3 +1,5 @@
+# using dijkstra's algorithm, extra dimension in the distance array to keep track of damage
+
 import sys
 import heapq
 
@@ -16,7 +18,7 @@ dist = [[inf]*201 for _ in range(n_islands + 1)]
 for i in range(201):
     dist[start][i] = 0  # dist to start is always 0
 
-pq = [(0, start, 0)]
+pq = [(0, start, 0)]  # (distance, island, damage)
 
 while pq:
     time, island, damage = heapq.heappop(pq)
@@ -35,7 +37,7 @@ while pq:
         if new_damage >= hull:  # broken hull, can't take this path
             continue
 
-        if new_time < dist[adj][new_damage]:
+        if new_time < dist[adj][new_damage]:  # found a shorter path
             dist[adj][new_damage] = new_time
             heapq.heappush(pq, (new_time, adj, new_damage))
 
