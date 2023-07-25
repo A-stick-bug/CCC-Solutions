1e633d74bf6c07f0ee2d8bf8c299807b462c81f8
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-24 20:16:19 -0400

    comment change

--- 2001/CCC '01 S5 - Post's Correspondence Problem.py
+++ 2001/CCC '01 S5 - Post's Correspondence Problem.py
@@ -1,4 +1,4 @@
-# extremely inefficient and lazy solution, barely passes, 0.05 seconds from TLE
+# extremely inefficient and lazy solution, barely passes, 0.05 seconds from time limit
 
 from itertools import product
 import sys
@@ -9,17 +9,17 @@ a = [input() for _ in range(N)]
 b = [input() for _ in range(N)]
 
 seq = list(range(N))
-all_combinations = []
+all_combinations = []  # generate every possible sequence
 for i in range(1,M):
     all_combinations.extend(list(product(seq, repeat=i)))
 
-for comb in all_combinations:
+for comb in all_combinations:  # test all of them
     s1 = [a[i] for i in comb]
     s2 = [b[i] for i in comb]
     if "".join(s1) == "".join(s2):
         print(len(comb))
         for i in comb:
-            print(i+1)  # +1 because question uses 1 indexing so
+            print(i+1)  # +1 because question uses 1 indexing
         sys.exit()
 
 print("No solution.")
