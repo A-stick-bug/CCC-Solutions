5578d742233833c59e45ac1cb96e1574a4065729
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-30 21:50:26 -0400

    improved to O(n)

--- 2007/CCC '07 J4 - Anagram Checker.py
+++ 2007/CCC '07 J4 - Anagram Checker.py
@@ -1,20 +1,23 @@
-a = input()
-b = input()
+# looking back at my horribly inefficient solution bothered me, so I improved it
+# now, it's O(n)
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-alphabet = alphabet.upper()
+from collections import defaultdict
 
-x = True
+# if they have the same amount of each letter, they are anagrams
+s1 = input()
+s2 = input()
 
-for i in alphabet:
-    num_a = a.count(str(i))
-    num_b = b.count(str(i))
+count1 = defaultdict(int)
+for i in s1:
+    if i != " ":  # ignore spaces
+        count1[i] += 1
 
-    if int(num_a) != int(num_b):
-        x = False
-        break
+count2 = defaultdict(int)
+for i in s2:
+    if i != " ":
+        count2[i] += 1
 
-if x:
+if count1 == count2:
     print("Is an anagram.")
 else:
     print("Is not an anagram.")
