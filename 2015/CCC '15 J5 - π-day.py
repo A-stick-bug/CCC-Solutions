006b6d637a2da80af4b6c5de33b5a1b1e70aa834
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-03 14:22:14 -0400

    added bottom up approach

--- 2015/CCC '15 J5 - π-day.py
+++ 2015/CCC '15 J5 - π-day.py
@@ -39,3 +39,18 @@ def solve(state: list, total: int):
 
 
 print(solve([0], 0))
+
+
+# # more efficient, but less intuitive, tabulation (bottom-up) approach, 15/15
+# n = int(input())
+# people = int(input())
+# dp = [[0 for _ in range(people + 1)] for _ in range(n + 1)]
+#
+# for i in range(1, people + 1):
+#     dp[i][i] = 1
+#
+# for i in range(1, n + 1):
+#     for j in range(1, min(i, people + 1)):
+#         dp[i][j] += dp[i - 1][j - 1] + dp[i - j][j]
+#
+# print(dp[n][people])
