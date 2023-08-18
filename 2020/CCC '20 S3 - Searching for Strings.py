029749cffc29e56f20d253a731642e4552b5750e
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-18 15:21:09 -0400

    broken

--- 2020/CCC '20 S3 - Searching for Strings.py
+++ 2020/CCC '20 S3 - Searching for Strings.py
@@ -3,22 +3,31 @@
 from collections import Counter
 
 needle = input()
-needle_count = Counter(needle)
+permutation = Counter(needle)
 haystack = input()
 
 used = set()
 
 
 def solve():
+    sub = Counter(haystack[:len(needle)])
+
     res = 0
     if len(needle) > len(haystack):
         return 0
-    for i in range(0, len(haystack)-len(needle)+1):
-        window = tuple(haystack[i:i+len(needle)])
-        sub = Counter(window)
-        if needle_count == sub and window not in used:
-            res += 1
-            used.add(window)
+
+    for left in range(len(haystack) - len(needle) + 1):
+        right = left + len(needle)-1
+
+        if permutation == sub:
+            window = tuple(haystack[left:right+1])
+            if window not in used:
+                res += 1
+                used.add(window)
+
+        if right != len(haystack)-1:
+            sub[haystack[left]] -= 1
+            sub[haystack[right+1]] += 1
 
     return res
 
