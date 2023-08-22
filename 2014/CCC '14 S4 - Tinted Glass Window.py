f039212c47ddd96e2817e4de0e36bd62830ad36f
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-21 21:08:20 -0400

    Update CCC '14 S4 - Tinted Glass Window.py

--- 2014/CCC '14 S4 - Tinted Glass Window.py
+++ 2014/CCC '14 S4 - Tinted Glass Window.py
@@ -1,33 +1,41 @@
-n = int(input())
-threshold = int(input())
-x_coords = []
-y_coords = []
+from sys import stdin
 
-for i in range(n):
-    # read the coordinates and tint-factor of each piece of glass
-    x1,y1,x2,y2,tint = map(int, input().split())
-    x_coords.append([x1, y1, y2, tint])  # [x1, y1, x2, tint]
-    x_coords.append([x2, y1, y2, -tint]) # [x2, y1, x2, -tint]
-    y_coords.append(y1)
-    y_coords.append(y2)
+input = stdin.readline
 
-x_coords = sorted(x_coords)
-y_coords = sorted(list(set(y_coords)))
+N = int(input())
+T = int(input())
+px = set()  # set of points
+py = set()
+rects = []
 
-nums = [0 for x in range(len(y_coords) - 1)]
-size = len(nums)
-lx = 0
+for i in range(N):
+    x1, y1, x2, y2, t = map(int, input().split())
+    px.add(x1)
+    py.add(y1)
+    px.add(x2)
+    py.add(y2)
+    rects.append([x1, y1, x2, y2, t])
+
+px = sorted(list(px))
+py = sorted(list(py))
+cx = {px[i]: i for i in range(len(px))}  # compressed coordinates
+cy = {py[i]: i for i in range(len(py))}
+
+diff = [[0] * (len(px) + 1) for i in range(len(py) + 1)]
+
+for i in range(N):
+    x1, y1, x2, y2, t = rects[i]
+    x1, y1, x2, y2 = cx[x1], cy[y1], cx[x2], cy[y2]  # use compressed coordinates for difference array
+    diff[y1 + 1][x1 + 1] += t
+    diff[y2 + 1][x2 + 1] += t
+    diff[y1 + 1][x2 + 1] -= t
+    diff[y2 + 1][x1 + 1] -= t
+
 res = 0
-
-for i in x_coords:
-    for j in range(size):
-        if nums[j] >= threshold:
-            res += (y_coords[j + 1] - y_coords[j]) * (i[0] - lx)
-
-    # overlap
-    for j in range(size):
-        if i[1] <= y_coords[j] and i[2] >= y_coords[j + 1]:
-            nums[j] += i[3]
-    lx = i[0]
+for i in range(1, len(py)):
+    for j in range(1, len(px)):
+        diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1]
+        if diff[i][j] >= T:
+            res += (py[i] - py[i - 1]) * (px[j] - px[j - 1])  # use the real coordinates when adding tint
 
 print(res)
