d46d31886ab8d7d3d97fc5144803abe602239b87
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-22 21:25:39 -0400

    slight changes

--- 2014/CCC '14 S4 - Tinted Glass Window.py
+++ 2014/CCC '14 S4 - Tinted Glass Window.py
@@ -11,31 +11,32 @@ rects = []
 for i in range(N):
     x1, y1, x2, y2, t = map(int, input().split())
     px.add(x1)
-    py.add(y1)
     px.add(x2)
+    py.add(y1)
     py.add(y2)
     rects.append([x1, y1, x2, y2, t])
 
 px = sorted(list(px))
 py = sorted(list(py))
-cx = {px[i]: i for i in range(len(px))}  # compressed coordinates
-cy = {py[i]: i for i in range(len(py))}
+cx = {px[i - 1]: i for i in range(1, len(px) + 1)}  # compressed coordinates
+cy = {py[i - 1]: i for i in range(1, len(py) + 1)}
 
 diff = [[0] * (len(px) + 1) for i in range(len(py) + 1)]
 
 for i in range(N):
     x1, y1, x2, y2, t = rects[i]
     x1, y1, x2, y2 = cx[x1], cy[y1], cx[x2], cy[y2]  # use compressed coordinates for difference array
-    diff[y1 + 1][x1 + 1] += t
-    diff[y2 + 1][x2 + 1] += t
-    diff[y1 + 1][x2 + 1] -= t
-    diff[y2 + 1][x1 + 1] -= t
+    diff[y1][x1] += t
+    diff[y2][x2] += t
+    diff[y1][x2] -= t
+    diff[y2][x1] -= t
 
 res = 0
-for i in range(1, len(py)):
-    for j in range(1, len(px)):
+for i in range(1, len(py) + 1):
+    for j in range(1, len(px) + 1):  # convert back into regular array
         diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1]
         if diff[i][j] >= T:
+            # height * width for total area of this compressed region
             res += (py[i] - py[i - 1]) * (px[j] - px[j - 1])  # use the real coordinates when adding tint
 
 print(res)
