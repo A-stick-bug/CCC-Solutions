eaf6ec7e2987773d23c25bcc3502f7c3b342c94e
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-09-09 13:26:35 -0400

    slight changes

--- 2020/CCC '20 S2 - Escape Room.py
+++ 2020/CCC '20 S2 - Escape Room.py
@@ -1,3 +1,4 @@
+# method 2, 15/15: compress matrix graph into a 1D adjacency list
 import sys
 from collections import deque
 
@@ -6,16 +7,16 @@ COLS = int(input())
 end = ROWS * COLS
 graph = [[] for _ in range(end + 1)]
 
-# constructing the graph: element i has all factors of i that are also in the 'graph'
+# constructing the graph
 for r in range(1, ROWS + 1):
     row = map(int, input().split())
     c = 1
     for num in row:
         if num <= end:
-            graph[r * c].append(num)
+            graph[r * c].append(num)  # avoid having to factor
         c += 1
 
-# BFS
+# simple BFS on adjacency list
 visited = [False for _ in range(end + 1)]
 visited[1] = True
 q = deque([1])
@@ -35,23 +36,24 @@ while q:
 print("no")
 
 
-
-# 13/15 TLE
+# # Brute force, 13/15 TLE, this would pass if it was written in c++
+# # The cache part is very important
+#
 # from collections import deque
+# import sys
+# from functools import cache
+#
+# input = sys.stdin.readline
 #
 #
-# factors = {}
+# @cache
 # def find_pairs(num):
-#     if num in factors:
-#         return factors[num]
-#
 #     pairs = []
 #     for i in range(1, int(num ** 0.5) + 1):
 #         if num % i == 0:
 #             pairs.append((i, num // i))
 #             if i != num // i:
 #                 pairs.append((num // i, i))
-#     factors[num] = pairs
 #     return pairs
 #
 #
@@ -61,24 +63,25 @@ print("no")
 # for i in range(ROWS):
 #     grid.append(list(map(int, input().split())))
 #
-# queue = deque()
-# queue.append((1,1))
+# q = deque()
+# q.append((1, 1))
 # visited = [[False for _ in range(COLS)] for _ in range(ROWS)]
+# visited[0][0] = True
 # no = True
 #
-# while queue:
-#     cell = queue.popleft()
-#     row = cell[0]
-#     col = cell[1]
-#     visited[row-1][col-1] = True
+# while q:
+#     row, col = q.popleft()
+#
 #     if row == ROWS and col == COLS:
 #         print("yes")
 #         no = False
 #         break
 #
-#     pairs = find_pairs(grid[row-1][col-1])
+#     pairs = find_pairs(grid[row - 1][col - 1])
 #     for i, j in pairs:
-#         if 0 <= i-1 < ROWS and 0 <= j-1 < COLS and not visited[i-1][j-1]:
-#             queue.append((i,j))
+#         if 0 <= i - 1 < ROWS and 0 <= j - 1 < COLS and not visited[i - 1][j - 1]:
+#             visited[i - 1][j - 1] = True
+#             q.append((i, j))
 #
-# if no: print("no")
+# if no:
+#     print("no")
