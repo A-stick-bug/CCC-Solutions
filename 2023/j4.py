c6a58c148c3c6d2a705142307980dee5f0d4b45e
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-10-26 08:34:32 -0400

    made code cleaner

--- 2023/j4.py
+++ 2023/j4.py
@@ -1,32 +1,22 @@
-row_length = int(input())
-row1 = input().split()
-row2 = input().split()
+n = int(input())
+row1 = [0] + list(map(int, input().split()))
+row2 = [0] + list(map(int, input().split()))
 
-tape = 0
+total = 0
 
-if row1[0] == "1":
-    tape += 3
-for i in range(1, row_length):
-    if row1[i] == "1":
-        tape += 3
-        if row1[i - 1] == "1":
-            tape -= 2
+for i in range(1, n + 1):
+    if row1[i] == 1:
+        if row1[i - 1] == 1:
+            total += 1
+        else:
+            total += 3
 
-# bottom row touches top row is index is EVEN
-if row2[0] == "1":
-    if row1[0] == "1":
-        tape += 1
-    else:
-        tape += 3
+for i in range(1, n + 1):
+    if row2[i] == 1:
+        total += 3
+        if row2[i - 1] == 1:
+            total -= 2
+        if i % 2 == 1 and row1[i] == 1:
+            total -= 2
 
-for i in range(1, row_length):
-    if row2[i] == "1":
-        tape += 3
-        if i % 2 == 0:  # check if touches top
-            if row1[i] == "1":
-                tape -= 2
-
-        if row2[i - 1] == "1":
-            tape -= 2
-
-print(tape)
+print(total)
