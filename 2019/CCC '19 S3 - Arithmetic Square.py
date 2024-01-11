fd9d1e36ff79f8d44ff4b0c552aba83662686706
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-01-11 16:24:46 -0500

    troll problem, just use random

--- 2019/CCC '19 S3 - Arithmetic Square.py
+++ 2019/CCC '19 S3 - Arithmetic Square.py
@@ -1,43 +1,29 @@
-# time-consuming question, and lots of case work
-# got 13/15 because good rng
+# First fill obvious cases: in a row/column, there is 1 unknown
+# Then try to randomly fill in values until you get a working board
 
-# let x be the number of unknown numbers
-
-# subtask 1
-# if x <= 3, we can just use loops and keep on filling rows with only 1 'X' until we get the answer
-
-# subtask 3
-# if x >= 8 (at most 1 known number), we can use the same number
-# if x == 7, slightly more complicated, 2 cases: same row or different row
-
-# all subtasks (4-6 X)
-# for many possible placements of X, we can just use the fill_certain function up to 6 times to get an answer
-# if not, we can fill the middle number with ANYTHING (you can try this out by hand)
-
-import sys
-from random import randint  # :)
+from random import randint
 
 
 # check if the current grid is valid
 def is_correct(ans):
-    if count_x(ans) != 0:
+    if count_x(ans) != 0:  # make sure it is filled
         return False
 
     for i in range(3):
         for j in range(3):
-            if grid[i][j] != "X" and grid[i][j] != ans[i][j]:
+            if grid[i][j] != "X" and grid[i][j] != ans[i][j]:  # make sure we didn't modify the original board
                 return False
 
     return all([row[1] - row[0] == row[2] - row[1] for row in ans]) \
-        and all([col[1] - col[0] == col[2] - col[1] for col in rotate(ans)])
+        and all([col[1] - col[0] == col[2] - col[1] for col in rotate(ans)])  # check for arithmetic sequences
 
 
-# main function for solving the grid
-def fill_certain():  # for filling rows with only 1 unknown
+def fill_certain():
+    """main function for solving the grid, fills all ROWS with 1 unknown"""
     for i in range(3):
         if res[i].count("X") == 1:
             pos = res[i].index("X")
-            if pos == 0:
+            if pos == 0:  # basic math of arithmetic sequences
                 res[i][0] = res[i][1] - (res[i][2] - res[i][1])
             elif pos == 2:
                 res[i][2] = res[i][1] + (res[i][1] - res[i][0])
@@ -47,74 +33,38 @@ def fill_certain():  # for filling rows with only 1 unknown
 
 def fill_all():  # tries to fill everything
     global res
-    for _ in range(6):  # there are 6 rows/columns total which is how many times we must fill at most
+    for _ in range(3):  # there are 3 rows/columns total which is how many times we must fill at most
         fill_certain()
         res = rotate(res)  # rotate to do columns
         fill_certain()
         res = rotate(res)  # rotate it back
 
 
-def get_n():  # get coordinates that are not x
-    x = []
-    for i in range(3):
-        for j in range(3):
-            if res[i][j] != "X":
-                x.append((i, j))
-    return x
+# small utility functions to clean up code
+count_x = lambda grid: sum([row.count("X") for row in grid])  # total number of 'X' in board
+rotate = lambda grid: list(map(list, list(zip(*grid))))  # columns <-> rows, is its own inverse
+deepcopy = lambda x: [i.copy() for i in x]
 
-
-count_x = lambda grid: sum([row.count("X") for row in grid])  # total number of 'X' in input
-rotate = lambda grid: list(map(list, list(zip(*grid))))  # make the columns rows (90 degrees rotate)
-
-grid = [input().split() for _ in range(3)]
+grid = [input().split() for _ in range(3)]  # take input
 for i in range(3):
     for j in range(3):
         if grid[i][j] != "X":
             grid[i][j] = int(grid[i][j])
-res = [i.copy() for i in grid]
 
-x = count_x(grid)
-if x < 7:
-    fill_all()
+res = [i.copy() for i in grid]  # make a copy since we might need to compare to our original grid
+fill_all()  # fill trivial cases
 
-elif x >= 8:  # use same number (d = 0)
-    a = 1  # placeholder
-    for row in grid:
-        for num in row:
-            if num != "X":
-                a = num
-    res = [[a] * 3 for _ in range(3)]
-
-elif x == 7:  # 2 unknowns
-    rotated = False
-    if any([row.count("X") < 2 for row in grid]):  # if the 2x are on the same column, we make it rows instead
-        res = rotate(res)
-        rotated = True
-    x1, x2 = get_n()
-    r1, c1 = x1
-    r2, c2 = x2
-    res[r1] = [res[r1][c1]] * 3
-    res[r2] = [res[r2][c2]] * 3
-
-    fill_all()  # now we can determine the numbers in every cell
-
-# for 4-6, we can put anything in the middle
-# we might have to try a few times because of decimals, it will eventually give the answer though
+# start generating random stuff and hope it works
+restore = deepcopy(res)
 while not is_correct(res):
-    if res[1][1] == "X":
-        res[1][1] = randint(-1000, 1000)
-        fill_all()
-        if not is_correct(res):
-            res[1][1] = "X"  # didn't work, reset it
+    res = deepcopy(restore)  # didn't work, reset state
+    for i in range(3):  # try to fill unknown states
+        for j in range(3):
+            if res[i][j] != 'X':
+                continue
+            res[i][j] = randint(-1000, 1000)
+            fill_all()
 
-            for _ in range(4):
-                if res[0][0] == "X":
-                    res[0][0] = randint(-1000, 1000)
-                    fill_all()
-                    if not is_correct(res):
-                        res[0][0] = "X"
-                res = list(map(list, list(zip(*res[::-1]))))  # 90 degree rotate
-
-assert is_correct(res)
+assert is_correct(res)  # for testing purposes, makes sure the answer is correct
 for r in res:
     print(*r)
