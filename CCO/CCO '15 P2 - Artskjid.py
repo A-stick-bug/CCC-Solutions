de146de636c5ea8a6ff7e9404656373d9984366c
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-01-23 19:49:28 -0500

    Update CCO '15 P2 - Artskjid.py

--- CCO/CCO '15 P2 - Artskjid.py
+++ CCO/CCO '15 P2 - Artskjid.py
@@ -1,6 +1,7 @@
-# NP problem go brr
-# 48/50 using dp, check c++ code for 50/50 solution
-from collections import defaultdict
+# 49/50 using bitmask DP
+# Even though the time complexity is exponential, there are still overlapping states so we can use DP
+# Instead of storing each state as a list of True/False, we can use a number (0 and 1 in binary) to
+# represent if a node has been visited
 
 n_nodes, n_roads = map(int, input().split())
 graph = [[] for _ in range(n_nodes)]
@@ -10,30 +11,31 @@ for _ in range(n_roads):
     graph[a].append((b, dist))
 
 res = 0
-max_dists = defaultdict(lambda: defaultdict(int))
+max_dists = [[0] * (2 ** 18) for _ in range(n_nodes)]
 
-stack = [(0, 1, 0)]
+stack = [(0, 1, 0)]  # current node, path dp state, distance
 while stack:
     node, path, dist = stack.pop()
-    if node == n_nodes - 1:
+    if node == n_nodes - 1:  # new way to get to end, update answer
         res = max(res, dist)
         continue
 
     for adj, adj_dist in graph[node]:
         new_dist = dist + adj_dist
-        if path & (1 << adj):
+        if path & (1 << adj):  # already visited this node on the current path
             continue
 
-        new_path = path | (1 << adj)
+        new_path = path | (1 << adj)  # mark as visited in current path
 
-        if new_dist > max_dists[adj][new_path]:
+        if new_dist > max_dists[adj][new_path]:  # found better path
             max_dists[adj][new_path] = new_dist
             stack.append((adj, new_path, new_dist))
 
 print(res)
 
-
 # # 43/50 test cases, TLE
+# # store each state as (current node, visited nodes)
+#
 # n_nodes, n_roads = map(int, input().split())
 # graph = [[] for _ in range(n_nodes)]
 #
