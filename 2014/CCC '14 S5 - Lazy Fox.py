f533ff982f6252dec5f785347068a8c259a935c1
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-02-03 20:18:39 -0500

    tle 9/15

---
+++ 2014/CCC '14 S5 - Lazy Fox.py
@@ -0,0 +1,23 @@
+# 9/15
+# longest decreasing path: brute force dfs + memoization
+# TC: O(N^3), total of N^2 states with O(N) transitions
+
+from functools import cache
+
+n = int(input())
+points = [(0, 0)] + [tuple(map(int, input().split())) for _ in range(n)]
+
+dist = lambda a, b: (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1])
+
+
+@cache
+def solve(cur, md):
+    best = 0
+    for adj in range(1, n + 1):
+        d = dist(points[cur], points[adj])
+        if md > d:
+            best = max(best, 1 + solve(adj, d))
+    return best
+
+
+print(solve(0, 1 << 60) - 1)  # -1 because at the end, we end up going to the same node twice
