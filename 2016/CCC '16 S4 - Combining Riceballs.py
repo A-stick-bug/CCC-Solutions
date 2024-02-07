0ac0075c662da8412fa415f2ee022d8f12e77082
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-02-07 12:02:24 -0500

    rewrote solution

--- 2016/CCC '16 S4 - Combining Riceballs.py
+++ 2016/CCC '16 S4 - Combining Riceballs.py
@@ -1,38 +1,53 @@
-# nerd DP solution (submit with pypy3 on dmoj) 15/15
+"""
+https://dmoj.ca/problem/ccc16s4
 
-N = int(input())
-dp = [[0 for _ in range(402)] for _ in range(402)]
-total = [0 for _ in range(402)]
+Interval DP
+- Can combine equal adjacent: [1, 1] -> 2
+- Can combine [x, y, x]: [5, 7, 5] -> 17
+- Goal is to make the largest possible
+- Start with smaller intervals first, so we already have their value when using them for larger ones
 
-res = 0
-balls = list(map(int, input().split()))
-for i in range(N):
-    dp[i][i] = balls[i]
-    res = max(res, dp[i][i])
-    if i == 0:
-        total[0] = dp[i][i]
-    else:
-        total[i] = total[i - 1] + dp[i][i]
+TC: O(N^3)
+"""
 
-for length in range(1, N):
-    for l in range(N - length):
-        r = l + length
-        j = l + 1
-        k = r
-        while j <= k:
-            if dp[l][j - 1] and dp[l][j - 1] == dp[k][r] and (j == k or dp[j][k - 1]):
-                dp[l][r] = max(dp[l][r], dp[l][j - 1] + dp[j][k - 1] + dp[k][r])
-                res = max(res, dp[l][r])
+from itertools import accumulate
+
+n = int(input())
+arr = list(map(int, input().split()))
+psa = [0] + list(accumulate(arr))
+query = lambda l, r: psa[r + 1] - psa[l]
+
+dp = [[0] * n for _ in range(n)]  # dp[i][j] is whether we can combine all of [i,j] into 1 ball
+for i in range(n):
+    dp[i][i] = 1
+
+for length in range(1, n):  # start with smaller intervals
+    for i in range(n - length):
+        j = i + length
+        # Find (low,high) where sum[i,low] == sum[high,j], so we can combine all into [i,j]
+        # we also need to make sure every interval we are trying to combine can be turned into 1 ball
+        # Do this in O(n) using 2 pointers
+        low = i
+        high = j
+        while high > low:  # note: the middle interval may not exist, but we can still combine low and high
+            if (query(i, low) == query(high, j) and (low == high - 1 or dp[low + 1][high - 1]) and
+                    dp[i][low] and dp[high][j]):
+                dp[i][j] = 1
                 break
-            if total[j - 1] - total[l - 1] < total[r] - total[k - 1]:
-                j += 1
-            else:
-                k -= 1
+            elif query(i, low) > query(high, j):  # left has greater sum, we need to add more to the right
+                high -= 1
+            else:  # add more to the left
+                low += 1
 
-print(res)
+# get best answer
+best = 0
+for i in range(n):
+    for j in range(n):
+        if dp[i][j]:
+            best = max(best, query(i, j))
+print(best)
 
-# brute force 3/15 TLE (very easy to understand)
-
+# # brute force 3/15 TLE (very easy to understand)
 # N = int(input())
 # balls = tuple(map(int, input().split()))
 #
