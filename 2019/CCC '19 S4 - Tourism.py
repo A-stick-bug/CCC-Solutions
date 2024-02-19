35e5539ef86302fa751858fd1c4b8f31be9e0632
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-02-19 14:52:00 -0500

    Update CCC '19 S4 - Tourism.py

--- 2019/CCC '19 S4 - Tourism.py
+++ 2019/CCC '19 S4 - Tourism.py
@@ -1,16 +1,21 @@
-# THIS IS NOT THE FULL SOLUTION
+# WORK IN PROGRESS
 #
 # https://dmoj.ca/problem/ccc19s4
 # part of my pre-2024 CCC partial mark grind
 # will probably add full solution later
+# at most 1.5N dp cells will be used
 
-n, k = map(int, input().split())
-arr = list(map(int, input().split()))
+# n, k = map(int, input().split())
+# arr = list(map(int, input().split()))
+n = 1000
+k = 2
+arr = [1] * n
 
 MAX_DAY = (n + k - 1) // k  # "using fewest days possible" means we must use this many days
+inf = 1 << 30
 
 # notice that the 2 subtasks don't overlap, this is subtask 1
-if 1 and 2 * k >= n:
+if 2 * k >= n:
     prefix, suffix, both = [0], [0], [0]
     for i in range(n):
         if i < k and (n - i - 1) < k:
@@ -32,23 +37,22 @@ if 1 and 2 * k >= n:
     print(max(bp + bs, bp + bb1, bs + bb1, bb1 + bb2))
 
 # brute force DP, subtask 2
-# transition: dp[i][k] -> dp[i+1][]
 else:
-    from functools import cache
-    @cache
-    def solve(i, day):
-        if day > MAX_DAY:
-            return -(1<<60)
-        if i == n:
-            if day == MAX_DAY:
-                return 0
-            else:
-                return -(1<<60)
-        cm = 0
-        best = 0
-        for j in range(i, min(n,i+k)):
-            cm = max(cm, arr[j])
-            best = max(best, cm + solve(j+1, day + 1))
-        return best
+    dp = [[-inf] * (MAX_DAY + 1) for _ in range(n + 1)]
+    dp[n][MAX_DAY] = 0  # base case
 
-    print(solve(0, 0))
+    for i in reversed(range(n)):
+        for day in reversed(range(MAX_DAY)):
+            # if not i*k <=
+            cm = 0
+            for j in range(i, min(n, i + k)):
+                cm = max(cm, arr[j])
+                dp[i][day] = max(dp[i][day], cm + dp[j + 1][day + 1])
+
+    print(dp[0][0])
+
+    t = 0
+    for i in range(n + 1):
+        for j in range(MAX_DAY + 1):
+            t += dp[i][j] == -inf
+    print(t)
