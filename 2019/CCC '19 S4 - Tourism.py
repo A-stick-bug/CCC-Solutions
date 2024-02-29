05741096a8e2fd49e23b5adaf3c0768e159ea3df
Ivan Li <104738598+A-stick-bug@users.noreply.github.com>
2024-02-29 17:49:16 -0500

    Update CCC '19 S4 - Tourism.py

--- 2019/CCC '19 S4 - Tourism.py
+++ 2019/CCC '19 S4 - Tourism.py
@@ -1,15 +1,8 @@
 # WORK IN PROGRESS
-#
-# https://dmoj.ca/problem/ccc19s4
-# part of my pre-2024 CCC partial mark grind
-# will probably add full solution later
-# at most 1.5N dp cells will be used
+# 5/15
 
-# n, k = map(int, input().split())
-# arr = list(map(int, input().split()))
-n = 1000
-k = 2
-arr = [1] * n
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
 
 MAX_DAY = (n + k - 1) // k  # "using fewest days possible" means we must use this many days
 inf = 1 << 30
@@ -36,23 +29,3 @@ if 2 * k >= n:
 
     print(max(bp + bs, bp + bb1, bs + bb1, bb1 + bb2))
 
-# brute force DP, subtask 2
-else:
-    dp = [[-inf] * (MAX_DAY + 1) for _ in range(n + 1)]
-    dp[n][MAX_DAY] = 0  # base case
-
-    for i in reversed(range(n)):
-        for day in reversed(range(MAX_DAY)):
-            # if not i*k <=
-            cm = 0
-            for j in range(i, min(n, i + k)):
-                cm = max(cm, arr[j])
-                dp[i][day] = max(dp[i][day], cm + dp[j + 1][day + 1])
-
-    print(dp[0][0])
-
-    t = 0
-    for i in range(n + 1):
-        for j in range(MAX_DAY + 1):
-            t += dp[i][j] == -inf
-    print(t)
