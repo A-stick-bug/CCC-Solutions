2da5c0eb5eb69313a7e3edab45b718b0eef78a37
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-03-10 12:15:27 -0400

    simplification

--- 2024/CCC '24 S3 - Swipe.py
+++ 2024/CCC '24 S3 - Swipe.py
@@ -1,13 +1,17 @@
-# https://dmoj.ca/problem/ccc24s3
-# credit to yujhtheyujh for ideas and sharing his clever way of thinking
-# consider this question to be crossing lines between the 2 lists without intersecting any
-#
-# observations: left and right swipes NEVER overlap
-# note to self: add proper explanation and simplify solution later
+"""
+https://dmoj.ca/problem/ccc24s3
+credit to yujhtheyujh for ideas and sharing his clever way of thinking
+consider this question to be crossing lines between the 2 lists without intersecting any
+
+observations:
+- left and right swipes NEVER overlap
+- right swipes must be output in reversed order to prevent overriding values
+  - other than this, the order of swipes doesn't matter
+"""
 
 n = int(input())
-arr = list(map(int, input().split()))
-target = list(map(int, input().split()))
+arr = input().split()
+target = input().split()
 
 swipes_l = []
 swipes_r = []
@@ -33,12 +37,8 @@ for i in range(n):
 
 if j == n:
     print("YES")
-
-    # output in correct order, that's the annoying part of this method
     print(len(swipes_l) + len(swipes_r))
-    swipes_r.sort(key=lambda x: x[1], reverse=True)
-    swipes_l.sort(key=lambda x: x[0])
-    for i in swipes_r:
+    for i in reversed(swipes_r):  # output right swipes in reverse order to prevent overriding values
         print("R", i[0], i[1])
     for i in swipes_l:
         print("L", i[0], i[1])
@@ -47,6 +47,13 @@ else:
     print("NO")
 
 """
+5
 1 3 3 2 1
 1 1 2 1 1
+
+YES
+3
+R 0 1
+L 2 3
+L 3 4
 """
