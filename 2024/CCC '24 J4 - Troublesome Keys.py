e32fd5865a4b6ab55976d75d72f0a34c3634cd95
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-03-12 20:03:03 -0400

    better solution

--- 2024/CCC '24 J4 - Troublesome Keys.py
+++ 2024/CCC '24 J4 - Troublesome Keys.py
@@ -1,32 +1,41 @@
 """
-Important: Alex never presses the silly key immediately after pressing the quiet key
+https://dmoj.ca/problem/ccc24j4
+Using logic reasoning and a bit of implementation
+The question gives up many constraints on the 2 strings, so there are many methods of solving this.
+
+Note the following:
+- "Alex never actually tries to type the wrong letter displayed by the silly key"
+- "Alex presses the silly key at least once"
+Using these 2 statements, we can reason that the character that only exists in the second string must
+be the result of the silly key
+
 """
 
-s = input() + "."
-t = input() + "."
+s = input() + "."  # original string
+t = input() + "."  # resulting string
 
 quiet = '-'
-silly = ['-', '-']
+silly = ['-', '-']  # [original character, resulting character]
+
+for c in "qwertyuiopasdfghjklzxcvbnm":  # find the result of the silly key
+    if c in t and c not in s:
+        silly[1] = c
 
 i = j = 0
 while i < len(s) and j < len(t):
-    if s[i] == t[j]:
+    if s[i] == t[j]:  # same letter, don't do anything
         i += 1
         j += 1
         continue
 
-    if s[i] == quiet:
-        i += 1
-
-    elif s[i + 1] == t[j]:  # skipped a letter
-        quiet = s[i]
-        i += 1
-
-    else:  # replaced a letter
+    if t[j] == silly[1]:  # result is silly key, s[i] must be the original character then
         silly[0] = s[i]
-        silly[1] = t[j]
         i += 1
         j += 1
+
+    else:  # result is NOT the silly key, it must be the quiet key then
+        quiet = s[i]
+        i += 1  # only increment i since the quiet key doesn't result in any characters being typed
 
 print(*silly)
 print(quiet)
