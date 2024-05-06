7ba0d21938f9e8ba99924f48b0f7d2c32504627d
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-05-06 17:16:23 -0400

    solved properly

--- CCO/CCO '07 P2 - Snowflakes.py
+++ CCO/CCO '07 P2 - Snowflakes.py
@@ -1,13 +1,14 @@
 """
+https://dmoj.ca/problem/cco07p2
 There are 12 ways of representing each snowflake and storing all of them in a set is too memory consuming
 Instead, we find a way to turn all rotations into the same form such as by creating all 12 ways and
 storing the lexicographically smallest one
 
-note: in my implementation, I just put the biggest number first, which technically doesn't work on a case like
+Example case:
 2
 2 2 1 2 2 2
 2 1 2 2 2 2
-but the test cases are weak, so it passes (as of 2023/11/10)
+"No two snowflakes are alike."
 """
 
 import sys
@@ -20,16 +21,13 @@ for _ in range(n):
     flake = list(map(int, input().split()))
     rev = list(reversed(flake))
 
-    mi = flake.index(max(flake))
-    ri = rev.index(max(rev))
-    c = ",".join(map(str, flake[mi:] + flake[:mi]))  # put maximum element at the start, passes due to weak test cases
-    r = ",".join(map(str, rev[ri:] + rev[:ri]))
+    # take lexicographical minimum rotation/reverse
+    standard = min([flake[i:] + flake[:i] for i in range(6)] + [rev[i:] + rev[:i] for i in range(6)])
+    standard = tuple(standard)  # turn to tuple since lists are not hashable
 
-    if r in seen or c in seen:
+    if standard in seen:
         print("Twin snowflakes found.")
         sys.exit()
-
-    seen.add(r)
-    seen.add(c)
+    seen.add(standard)
 
 print("No two snowflakes are alike.")
