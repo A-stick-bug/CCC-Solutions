58d7086f03680a1911a31d5644c802dcb2b2fe8d
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-05-23 09:19:36 -0400

    improved

--- 2003/CCC '03 S4 - Substrings.py
+++ 2003/CCC '03 S4 - Substrings.py
@@ -1,9 +1,31 @@
-# brute force solution (passes half the test cases)
+# 70/100
+# improved to O(N^2), might add full solution later
+
+def solve():
+    s = input()
+    trie = {}
+
+    def add_word(word):
+        cur = trie
+        for char in word:
+            if char in cur:
+                cur = cur[char]
+            else:
+                cur[char] = {}
+                cur = cur[char]
+
+    for i in range(len(s)):
+        add_word(s[i:])
+
+    tot = 0
+    stack = [trie]
+    while stack:
+        cur = stack.pop()
+        tot += 1
+        for adj in cur.values():
+            stack.append(adj)
+    print(tot)
+
 
 for _ in range(int(input())):
-    s = input()
-    res = set()
-    for i in range(len(s)):
-        for j in range(i + 1, len(s) + 1):
-            res.add(s[i:j])
-    print(len(res)+1)
+    solve()
