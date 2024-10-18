9b7e2b0d85bcb3c47e92912faa06c99ed9731974
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-10-17 22:32:37 -0400

    simplified solution + add explanation

--- 2022/CCC '22 S3 - Good Samples.py
+++ 2022/CCC '22 S3 - Good Samples.py
@@ -1,42 +1,47 @@
-# for these type of questions, solve the small cases by hand and look for patterns
-# create a function for generating random cases and checking for correctness if needed (code for this at the bottom)
-# ad-hoc, constructive, greedy algorithms
+"""
+https://dmoj.ca/problem/ccc22s3
+
+Observations:
+- a cyclic pattern from 1-M e.g. [1,2,3,1,2,3,1,2,3,...] gives the most good samples
+- adding an element will make at least 1 good sample
+
+Strategy:
+- start with K - N, then add 1 every iteration, so we don't overshoot the good sample amount
+  if we have enough good samples, K will be 1 at the start of every iteration so we just add the previous number
+- first add as many good samples as possible
+- at the end, only add the necessary amount of good samples
+"""
 
 import sys
 
 n, m, k = map(int, input().split())
-# assert n >= m  # for testing purposes
 
-calc = lambda x: (x * (x + 1)) // 2
-d = n - m
-max_samples = calc(n) - calc(d)  # maximum number of good samples possible
-
-if k > max_samples or k < n:  # there are at least n good samples (all numbers are the same)
+if k < n:  # there are at least n good samples (all numbers are the same)
     print(-1)
     sys.exit()
 
 k -= n
 res = []
-good = 0
+
 for i in range(1, n + 1):
     k += 1  # add one by one because adding any number will increase the number of good samples by 1
 
-    if good == k:  # already have the right amount of good samples
-        res.append(res[-1])
-
-    elif i <= m and good + i <= k:  # need more samples, add the number that gives the most extra samples
+    if i <= m and i <= k:  # need more samples, add the number that gives the most extra samples
         res.append(i)
-        good += i
+        k -= i
 
-    elif i > m and good + m <= k:  # need more samples, max pitch reached, now cycling through 1 to m
+    elif i > m and m <= k:  # need more samples, max pitch reached, now cycling through 1 to m
         res.append(m if i % m == 0 else i % m)
-        good += m  # when cycling through 1 to m, you get m new good samples for every note
+        k -= m  # when cycling through 1 to m, you get m new good samples for every note
 
     else:  # almost right, instead of adding maximum samples, add just the right amount
-        diff = k - good
-        res.append(res[-diff])
+        res.append(res[-k])
+        k = 0
 
-print(*res)
+if k != 0:
+    print(-1)  # not enough good samples
+else:
+    print(*res)
 
 # # used for checking if the output is actually correct
 #
