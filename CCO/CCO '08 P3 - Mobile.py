ac6b78f42e6e0d191ae981e2d04bb303044845f6
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2025-04-17 21:43:37 -0400

    added more rigorous solution

--- CCO/CCO '08 P3 - Mobile.py
+++ CCO/CCO '08 P3 - Mobile.py
@@ -1,19 +1,11 @@
-# both mobiles can be thought of as a tree
-# it can be proven that if every layer of the 2 mobiles have the same amount of each ornament (order doesn't matter),
-# then there exists a way to rearrange them to match the other mobile (maybe?)
+# https://dmoj.ca/problem/cco08p3
+# Encode rooted tree recursively in a 'standardized' way and check equality
 
-# based on the fact that the test cases pass, this SHOULD be true, unsure about cases like this one though
-# 3
-# 2 3
-# -1 -2
-# -3 -4
-# 3
-# 2 3
-# -1 -3
-# -2 -4
-
-from collections import deque, defaultdict
 import sys
+
+MOD = 1_000_000_007
+base = 10_007
+encode = lambda num: num + base - 1
 
 input = sys.stdin.readline
 
@@ -22,42 +14,45 @@ n = int(input())
 graph = [[] for _ in range(n + 1)]
 for i in range(1, n + 1):
     l, r = map(int, input().split())
-    graph[i].extend([l, r])
+    graph[i] = [l, r]
 
 m = int(input())
 graph2 = [[] for _ in range(m + 1)]
 for i in range(1, m + 1):
     l, r = map(int, input().split())
-    graph2[i].extend([l, r])
+    graph2[i] = [l, r]
 
-# do a breadth first search of each layer on both trees and check if they match
-q = deque([1])
-q2 = deque([1])
 
-for layer in range(200_000):  # process each layer seperately
-    cur_layer = defaultdict(int)  # ornaments in the current layer (unordered)
-    cur_layer2 = defaultdict(int)
+def normalize(graph):
+    def solve(cur):
+        if cur < 0:  # leaf
+            return encode(cur)
+        l = solve(graph[cur][0])
+        r = solve(graph[cur][1])
 
-    if not q:  # already processed the entire thing and all layers matched
-        print("Fred and Mary might have the same mobile.")
-        break
+        # hopefully l != r when the subtrees are not equal
+        if l > r:
+            l, r = r, l  # generalize: smaller one first
+        return (l * base + r) % MOD
 
-    for i in range(len(q)):  # go through all elements in the current layer
-        node = q.popleft()
-        for adj in graph[node]:
-            if adj < 0:  # if the value is negative, it is an ornament which we need to match with the other tree
-                cur_layer[adj] += 1
-            else:
-                q.append(adj)
+    return solve(1)
 
-    for i in range(len(q2)):  # do the same thing for the other tree
-        node = q2.popleft()
-        for adj in graph2[node]:
-            if adj < 0:
-                cur_layer2[adj] += 1
-            else:
-                q2.append(adj)
 
-    if cur_layer != cur_layer2:  # if any layer doesn't match, we know that the 2 trees are different
-        print("Fred and Mary have different mobiles.")
-        break
+a = normalize(graph)
+b = normalize(graph2)
+if a == b:
+    print("Fred and Mary might have the same mobile.")
+else:
+    print("Fred and Mary have different mobiles.")
+
+"""
+3
+2 3
+-1 -2
+-3 -4
+3
+2 3
+-1 -3
+-2 -4
+different
+"""
